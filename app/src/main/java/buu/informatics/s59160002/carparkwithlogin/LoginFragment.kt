461deca0e5a8574e1c6d6f62a82e691fbe933f22
Jakgrit <jakgrit_frame@hotmail.com>
2019-09-16 15:44:36 +0700

    Add Btn

--- app/src/main/java/buu/informatics/s59160002/carparkwithlogin/LoginFragment.kt
+++ app/src/main/java/buu/informatics/s59160002/carparkwithlogin/LoginFragment.kt
@@ -2,12 +2,11 @@ package buu.informatics.s59160002.carparkwithlogin
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import buu.informatics.s59160002.carparkwithlogin.databinding.FragmentLoginBinding
 
 /**
@@ -25,8 +24,18 @@ class LoginFragment : Fragment() {
             view.findNavController().navigate(R.id.action_loginFragment2_to_carParkFragment2)
         }
 
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController() ) || super.onOptionsItemSelected(item)
+    }
 
 }
